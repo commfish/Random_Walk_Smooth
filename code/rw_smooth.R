18aa46e5cb8e36c25e8780c9c8cb946892bf1ea3
wgaeuman <william.gaeuman@alaska.gov>
2017-11-03 12:38:26 -0800

    Update rw_smooth.R

--- code/rw_smooth.R
+++ code/rw_smooth.R
@@ -14,9 +14,9 @@
 # 
 # Function inputs: 
 # s.yr		= survey years
-# b		= observed biomasses
+# b		= observed survey value, i.e. abunance/biomass
 # cv.b		= survey cvs
-# sigma		= process error (random walk standard deviation on log scale)
+# sigma		= assumed process error (random walk standard deviation on log scale)
 #
 # Defaults are PIGKC total biomass estimates from "biennial" NMFS shelf slope survey.
 # ----
@@ -29,7 +29,7 @@ rw.smooth <- function(s.yr = c(2002, 2004, 2008, 2010, 2012, 2016),
 n <- s.yr[length(s.yr)] - s.yr[1] + 1 		# number of years, e.g. 15
 s.index <- s.yr - s.yr[1] + 1			# survey year index, e.g. 1, 3, 7, 9, 11, 15	
 
-# Negative log likelihood function wrt predicted survey biomass b.hat
+# Negative log likelihood function wrt predicted survey abundance/biomass b.hat
 negll <- function(b.hat, sigma, b, cv.b, n, s.index){
 	var.log.b <- log(1 + cv.b^2)
 	sigma2 <- sigma^2
@@ -38,7 +38,7 @@ negll <- function(b.hat, sigma, b, cv.b, n, s.index){
 	value <- sum(log(2 * pi * var.log.b) + (log(b) - log(b.hat[s.index]))^2 / var.log.b)
 	
 	# random walk contribution
-	for(t in 2:n) value <- value + log(2* pi * sigma2) + (log(b.hat[t]) - log(b.hat[t-1]))^2 / sigma2
+	for(t in 2:n) value <- value + log(2 * pi * sigma2) + (log(b.hat[t]) - log(b.hat[t - 1]))^2 / sigma2
 	
 	return(0.5 * value)
 	}
